b579f39519022642e4840fd25e7f65438989d197
Ademola <a.g.jegede@wlv.ac.uk>
2022-04-07 09:29:29 +0100

    update with listing folder

---
+++ src/components/Listing/Listing.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import ListingItem from '../Listing-Item/ListingItem'
+import './Listing.css'
+
+function Listing() {
+  return (
+    <div className='Listing-section'>
+    
+    <ListingItem />
+    <ListingItem />
+    <ListingItem />
+    <ListingItem />
+    <ListingItem />
+    <ListingItem />
+    </div>
+  )
+}
+
+export default Listing
